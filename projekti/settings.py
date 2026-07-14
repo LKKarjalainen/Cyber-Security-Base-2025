aac94819f731f3c0e68e7b889f1f5c46c89b5513
LKKarjalainen <lassikarjalainen0@gmail.com>
2026-07-14 18:24:08 +0300

    flaw 5

--- projekti/settings.py
+++ projekti/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+# VULNERABLE: Security Misconfiguration (A02)
+
 
 from pathlib import Path
 
@@ -23,8 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vsh3a%6w774e2^$-_5r^4#t1!gu2(6pzjxc8g@3s9vb2yq^&c+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# Fix for A02
+#DEBUG = False
+#ALLOWED_HOSTS = ['*']
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
